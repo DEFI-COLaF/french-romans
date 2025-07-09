0dd9d622988d7ea19ed8fbd837136e17d47a17eb
Thibault Clérice <thibault.clerice@inria.fr>
2025-07-09 16:34:07 +0200

    Prework for Florian

--- step_02_svm.py
+++ step_02_svm.py
@@ -1,41 +1,95 @@
 import json
+import os
+import pickle
 from itertools import product
+from typing import Dict, Any, List
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import pandas as pd
-from tools.prepare import extract_all_authors_decade
+import numpy as np
+import tqdm
+from sklearn.preprocessing import StandardScaler
+from ruzicka.BDIVerifier import BDIVerifier
+
+from tools.prepare import extract_all_authors_decade, QueryCandidatesImpostors
+from tools.constants import rng
+from tools import compress
 
 # Set MIN_Candidates (used either in training set or as candidates for BPI)
 MIN_CANDIDATES = 1
+MAX_NUMBER_OF_SAMPLES = 50
+NB_PROCS = os.getenv("NBPROC", 4)
 
-# Load what was compiled in step_00
-with open("features.json") as f:
-    features = json.load(f)
-authors = pd.read_pickle("authors.pickle")
-impostors = pd.read_pickle("authors.pickle")
-authors_features = [feat for feat in authors.columns]
-impostors_features = [feat for feat in impostors.columns]
-metadata_cols = [col for col in authors.columns if col not in authors_features]
+# Define your subprocessed routine here
+def run_bdi(pickled_experiment: bytes) -> Dict[str, Any]:
+    import pickle  # Needed inside subprocess
+    experiment: QueryCandidatesImpostors = pickle.loads(pickled_experiment)
 
-# Now, we extract experiment situation from the author dataset
-for (gap, ascending) in product([0, 5, 10, 15], [True, False]):
-    for experiment in extract_all_authors_decade(
-            df=authors[metadata_cols+authors_features],
-            features=features,
-            gap=gap,
-            ascending=ascending,
-            min_candidates=1,
-            general_impostors=impostors[metadata_cols +impostors_features],
-            as_pickle=True
-        ):
-        # WE put everything together to all the same column order
-        def train(same_author, not_same):
-            return
-        def test(model, valid_test_data):
-            return
+    X = pd.concat([experiment.candidate[experiment.features], experiment.impostors[experiment.features]])
+    Y_raw = pd.concat([experiment.candidate["var_author"], experiment.impostors["var_author"]])
+    Y, label_uniques = Y_raw.factorize()
 
-        train(experiment.candidate, experiment.impostors)
-        test("", experiment.candidate)
-        # Note, you could also sample from experiment.impostors, to test precision
+    # We scale things as we should in BDI
+    scaler: StandardScaler = StandardScaler(with_mean=False).fit(X)
+    X = scaler.transform(X)
 
-        # Use the following for your logs:
-        #   experiment.author, experiment.year, experiment.gap
-        print("WOOHOOO")
+    # WE set up the BDI Verifier
+    bdi_mm = BDIVerifier(metric="minmax", nb_bootstrap_iter=1000, rnd_prop=.35, random_state=rng)
+    bdi_mm.fit(X, Y)
+
+    # We get our query scaled
+    query = scaler.transform(experiment.query[experiment.features].sample(
+        n=min([MAX_NUMBER_OF_SAMPLES, experiment.query.shape[0]]),
+        random_state=rng,
+        replace=True)
+    )
+    # Set labels as Filename:Window
+    labels = [f'{fn}#{w}' for w, fn in zip(experiment.query["var_window"].tolist(), experiment.query.index.tolist())]
+
+    # We query against candidate, hence the second list is a list of the author
+    proba = bdi_mm.predict_proba(query, [Y[0]] * (query.shape[0]))
+
+    return {
+        "arrays": np.round(bdi_mm._dist_arrays, decimals=4).tolist(),
+        "date": int(experiment.year),
+        "probas": np.round(proba, decimals=3).tolist(),
+        "labels": labels,
+        "gap": experiment.gap,
+        "author": experiment.author
+    }
+
+
+if __name__ == "__main__":
+    # Load what was compiled in step_00
+    with open("features.json") as f:
+        features = json.load(f)
+    authors = pd.read_pickle("authors.pickle")
+    impostors = pd.read_pickle("authors.pickle")
+    authors_features = [feat for feat in authors.columns]
+    impostors_features = [feat for feat in impostors.columns]
+    metadata_cols = [col for col in authors.columns if col not in authors_features]
+
+    # We add a TQDM progress bar
+    bar = tqdm.tqdm()
+
+    # Now, we extract experiment situation from the author dataset
+    for (gap, ascending) in product([1, 5, 10, 15], [True, False]):
+        results = []
+        # if os.path.exists(f"results-bdi-{gap}-{ascending}.json"):
+        #     continue
+        with ProcessPoolExecutor(max_workers=NB_PROCS) as executor:
+            futures = [
+                executor.submit(run_bdi, experiment)
+                for experiment in extract_all_authors_decade(
+                    df=authors[metadata_cols+authors_features],
+                    features=features,
+                    gap=gap,
+                    ascending=ascending,
+                    min_candidates=1,
+                    general_impostors=impostors[metadata_cols+impostors_features],
+                    as_pickle=True
+                )]
+            for future in as_completed(futures):
+                results.append(future.result())
+                bar.update(1)
+        compress.dump(results, f"results-bdi-{gap}-{ascending}.json")
+
